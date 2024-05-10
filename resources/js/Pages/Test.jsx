70a998499c2f659ee072ef6d87baef9a07673e86
PvtWendy <102826495+PvtWendy@users.noreply.github.com>
2024-05-10 15:17:56 -0400

    Setup do React.js, Inertia e Vite.js

---
+++ resources/js/Pages/Test.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+const Test = () => {
+    return (
+        <h1>Hello Charlotte</h1>
+    )
+}
+
+export default Test
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import refresh from "@vitejs/plugin-react-refresh";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
+        react(),
+        refresh(),
     ],
+    server: {
+        host: "localhost",
+        port: 8080, // Adjust port if needed
+    },
 });
