96bf31a3d65e9bcb696de3915731434fd524701b
PvtWendy <102826495+PvtWendy@users.noreply.github.com>
2024-05-10 16:10:26 -0400

    Mudança do placeholder para facilitar testes iniciais

--- resources/js/Pages/Test.jsx
+++ resources/js/Pages/Test.jsx
@@ -1,9 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Test = () => {
     return (
-        <h1 className='text-4xl text-red-400'>Hello Charlotte</h1>
-    )
-}
+        <div className="w-[30vw] h-[30vh] border-2 mx-auto my-[30vh] justify-center flex items-center">
+            <form action="" className="mx-auto w-fit flex flex-col gap-4 ">
+                <h1 className="w-fit">Hello Charlotte</h1>
+                <input type="text" className=" outline-gray-200 outline outline-1" placeholder="Placeholder"/>
+                 <input type="submit" value="Submit" className="hover:cursor-pointer bg-green-400 py-2 text-white rounded-sm hover:bg-green-600" />
+            </form>
+        </div>
+    );
+};
 
-export default Test
+export default Test;
