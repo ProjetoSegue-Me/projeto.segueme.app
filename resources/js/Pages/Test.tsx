d3ef090948df27e4d90dfdeeea4d3896646a5f12
PvtWendy <102826495+PvtWendy@users.noreply.github.com>
2024-05-24 17:11:29 -0400

    Alteração de extensão

---
+++ resources/js/Pages/Test.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import Header from "../Components/Header";
+
+export default function Test(){
+    return (
+        <div>
+            <Header/>
+            <div className="w-[30vw] h-[30vh] border-2 mx-auto my-[20vh] justify-center flex items-center">
+                <form action="" className="mx-auto w-fit flex flex-col gap-4 ">
+                    <h1 className="w-fit">Hello Charlotte</h1>
+                    <input
+                        type="text"
+                        className=" outline-gray-200 outline outline-1"
+                        placeholder="Placeholder"
+                    />
+                    <input
+                        type="submit"
+                        value="Submit"
+                        className="hover:cursor-pointer bg-green-400 py-2 text-white rounded-sm hover:bg-green-600"
+                    />
+                </form>
+            </div>
+        </div>
+    );
+};
