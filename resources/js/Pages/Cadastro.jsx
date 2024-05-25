5fe0741731086ae8ed2dcf657f76682e1e546cdf
PvtWendy <102826495+PvtWendy@users.noreply.github.com>
2024-05-25 15:25:53 -0400

    Criação inicial da pagina Cadastro

--- resources/js/Pages/Cadastro.jsx
+++ resources/js/Pages/Cadastro.jsx
@@ -1,12 +1,26 @@
 import React, { useState } from "react";
 import Header from "../Components/Header";
-
+import { InertiaLink } from "@inertiajs/inertia-react";
+import Titulo from "../Components/Titulo";
 export default function Cadastro() {
     return (
-        <div>
+        <div className="bg-[#F9CDA3]">
             <Header></Header>
-            <div className="w-[30vw] h-[30vh] border-2 mx-auto my-[20vh] justify-center flex items-center">
-                Redirecionado
+            <div className="mt-[5vw] flex flex-col justify-center">
+                <Titulo conteudo="Cadastrar"></Titulo>
+                <InertiaLink className="bg-white rounded-full px-[10vw] py-[0.75vw] mx-auto my-[2.5vw] w-fit text-[1.2vw] border-[1px] border-[#FFB718] hover:bg-[#ffead6] ease-in duration-100">
+                    Criar Cadastro
+                </InertiaLink>
+            </div>
+            <div>
+                <Titulo conteudo="Todos os Cadastros"></Titulo>
+                <div className="flex mx-auto w-fit">
+                    <div>
+                        <button className="bg-white rounded-l-full px-[10vw] py-[0.75vw] border-[1px] border-[#FFB718] text-[1.2vw] hover:bg-[#ffead6] ease-in duration-100">Tios/Tias</button>
+                        <button className="bg-white rounded-r-full px-[10vw] py-[0.75vw] border-[1px] border-[#FFB718] text-[1.2vw] hover:bg-[#ffead6] ease-in duration-100">Primos/Primas</button>
+                    </div>
+                    <button className="bg-white rounded-full"> <img src="/images/Pesquisa.png" className="w-[2w]"/> </button>
+                </div>
             </div>
         </div>
     );
