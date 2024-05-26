ad1edfa89066763909ae227ebe56185eba042d4f
PvtWendy <102826495+PvtWendy@users.noreply.github.com>
2024-05-25 21:39:30 -0400

    fix: Cores e tamanho de componentes

--- resources/js/Pages/Eventos.jsx
+++ resources/js/Pages/Eventos.jsx
@@ -9,10 +9,10 @@ export default function Eventos() {
             <div className="mt-[5vw] flex flex-col justify-center">
                 <Titulo conteudo="Criar Eventos"></Titulo>
                 <div className="flex flex-row mx-auto">
-                    <InertiaLink className="bg-white rounded-l-full w-[35vw] text-center py-[0.75vw]  my-[2.5vw]  text-[1.2vw] border-[1px] border-[#FFB718] hover:bg-[#ffead6] ease-in duration-100">
+                    <InertiaLink className="bg-[white] rounded-l-full w-[35vw] text-center py-[0.75vw]  my-[2.5vw]  text-[1.2vw] border-[1px] border-[#FFB718] hover:bg-[#ffead6] ease-in duration-100">
                         Criar Encontro
                     </InertiaLink>
-                    <InertiaLink className="bg-white rounded-r-full w-[35vw] text-center py-[0.75vw]  my-[2.5vw] text-[1.2vw] border-[1px] border-[#FFB718] hover:bg-[#ffead6] ease-in duration-100">
+                    <InertiaLink className="bg-[white] rounded-r-full w-[35vw] text-center py-[0.75vw]  my-[2.5vw] text-[1.2vw] border-[1px] border-[#FFB718] hover:bg-[#ffead6] ease-in duration-100">
                         Criar Retiro
                     </InertiaLink>
                 </div>
@@ -28,14 +28,14 @@ export default function Eventos() {
                 <Titulo conteudo="Eventos Finalizados"></Titulo>
                 <div className="flex mx-auto w-fit gap-[3vw] mt-[4vw]">
                     <div>
-                        <button className="bg-white rounded-l-full w-[35vw] py-[0.75vw] border-[1px] border-[#FFB718] text-[1.2vw] hover:bg-[#ffead6] ease-in duration-100">
+                        <button className="bg-[white] rounded-l-full w-[35vw] py-[0.75vw] border-[1px] border-[#FFB718] text-[1.2vw] hover:bg-[#ffead6] ease-in duration-100">
                             Encontros
                         </button>
-                        <button className="bg-white rounded-r-full w-[35vw] py-[0.75vw] border-[1px] border-[#FFB718] text-[1.2vw] hover:bg-[#ffead6] ease-in duration-100">
+                        <button className="bg-[white] rounded-r-full w-[35vw] py-[0.75vw] border-[1px] border-[#FFB718] text-[1.2vw] hover:bg-[#ffead6] ease-in duration-100">
                             Retiros
                         </button>
                     </div>
-                    <button className="bg-white rounded-full w-[4vw] border-[1px] border-[#FFB718] text-[1.2vw] hover:bg-[#ffead6] ease-in duration-100">
+                    <button className="bg-[white] rounded-full w-[4vw] h-[4vw] mt-[1vw] border-[1px] border-[#FFB718] text-[1.2vw] hover:bg-[#ffead6] ease-in duration-100">
                         <img
                             src="/images/Pesquisa.png"
                             className="h-[1.5vw] mx-auto"
