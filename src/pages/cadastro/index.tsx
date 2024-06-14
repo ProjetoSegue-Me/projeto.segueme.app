fd64b2b85e9d5f6c341f7ed451833038f1e8d491
PvtWendy <102826495+PvtWendy@users.noreply.github.com>
2024-06-14 16:25:36 -0400

    feat: Renderização dos cards dos membros

--- src/pages/cadastro/index.tsx
+++ src/pages/cadastro/index.tsx
@@ -2,9 +2,65 @@ import React, { useState } from "react";
 import Header from "@/components/Header";
 import Titulo from "@/components/Titulo";
 import { useRouter } from "next/navigation";
+import Card from "@/components/Card";
 
-export default function Cadastro() {
+const fetchAll = async () => {
+  try {
+    const response = await fetch(`http://localhost:3000/api/members/getAll`);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("There was a problem with the fetch operation:", error);
+    return null;
+  }
+};
+
+export async function getServerSideProps(context: any) {
+  const memberData = await fetchAll();
+
+  return {
+    props: { memberData },
+  };
+}
+
+function formatarTelefone(number: string) {
+  const cleaned = number.replace(/\D/g, "");
+
+  const formatted = `(${cleaned.substring(0, 2)}) ${cleaned.substring(
+    2,
+    7
+  )}-${cleaned.substring(7)}`;
+
+  return formatted;
+}
+
+const bufferImage = (bufferData: any) => {
+  const imageBase64 = Buffer.from(bufferData.data).toString("base64");
+  return "data:image/png;base64 ," + imageBase64;
+};
+
+export default function Cadastro({ memberData }: any) {
   const router = useRouter();
+
+  const renderMembers = () => {
+    return memberData.map((member: any) => (
+      <Card
+        key={member.idPessoa}
+        imageSource={bufferImage(member.foto)}
+        titulo={member.Conjuge ? "Tio/Tia" : "Primo/Prima"}
+        nome={member.NomeCompleto}
+        infoAdicional={
+          member.telefone[0]
+            ? formatarTelefone(member.telefone[0].Numero)
+            : "N/A"
+        }
+        rota={`/cadastro/detalhes/${member.idPessoa}`}
+      />
+    ));
+  };
   return (
     <div className="bg-bodyColor min-h-[100vh] overflow-hidden">
       <Header />
@@ -32,10 +88,15 @@ export default function Cadastro() {
             <img src="/images/Pesquisa.png" className="h-[1.5vw] mx-auto" />
           </button>
         </div>
-        {/*TODO: Fazer renderização condicional caso não tenha nenhum usuario (Depende do Back-end e banco de dados) */}
-        <h1 className="mx-auto w-fit font-roboto text-[1.5vw] py-[8vw]">
-          Não há nenhum membro cadastrado atualmente
-        </h1>
+        {!memberData ? (
+          <h1 className="mx-auto w-fit font-roboto text-[1.5vw] py-[8vw]">
+            Não há nenhum membro cadastrado atualmente
+          </h1>
+        ) : (
+          <div className="flex w-[90vw] mx-auto flex-wrap justify-between mt-[8vw] gap-y-[4vw]">
+            {renderMembers()}
+          </div>
+        )}
       </div>
     </div>
   );
