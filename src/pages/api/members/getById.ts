6ecb26c98690c14123bd2820bbf73c3aca91725d
PvtWendy <102826495+PvtWendy@users.noreply.github.com>
2024-06-13 22:58:51 -0400

    fix: Incluir dados não inclusos

--- src/pages/api/members/getById.ts
+++ src/pages/api/members/getById.ts
@@ -1,32 +1,39 @@
 // src/pages/api/pessoas/[id].ts verificar depois
 
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { PrismaClient } from '@prisma/client';
+import type { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const { id } = req.query;
 
-  if (req.method === 'GET') {
+  if (req.method === "GET") {
     try {
       const pessoa = await prisma.pessoa.findUnique({
         where: { idPessoa: Number(id) },
+        include: {
+          telefone: true,
+          escolaridade: true,
+        },
       });
 
       if (!pessoa) {
-        res.status(404).json({ error: 'Pessoa not found' });
+        res.status(404).json({ error: "Pessoa not found" });
       } else {
         res.status(200).json(pessoa);
       }
     } catch (error) {
       console.error(`Failed to fetch pessoa with id ${id}:`, error);
-      res.status(500).json({ error: 'Failed to fetch pessoa' });
+      res.status(500).json({ error: "Failed to fetch pessoa" });
     } finally {
       await prisma.$disconnect();
     }
   } else {
-    res.setHeader('Allow', ['GET']);
+    res.setHeader("Allow", ["GET"]);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
