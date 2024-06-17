ce4c3f73c6c7eb48c89c4ff1b22d9f024f651177
PvtWendy <102826495+PvtWendy@users.noreply.github.com>
2024-06-17 07:55:34 -0400

    feat: Adição de indicador de Carregamento

---
+++ src/components/Carregando.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Carregando() {
+    return (
+      <div className="fixed bottom-[1vw] right-[2vw] bg-white rounded-full border-colorStep border-2 text-center text-[1.2vw] py-[1vw] px-[1.5vw] z-30">
+        Carregando...
+      </div>
+    );
+  };
